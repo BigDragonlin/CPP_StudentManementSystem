5c9567bccf2b11a154b0e752660309fa80106dd5
meng <1363992060@qq.com>
2023-04-29 23:50:38 +0800

    学生管理系统:输出打印

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <d3dx9anim.h>
 
 struct stu {
     int num;
@@ -8,65 +9,70 @@ struct stu {
     struct stu *next;
 };
 
-stu * find() {
+stu *find() {
     return 0;
 }
 
-stu * add() {
+stu *add() {
     return 0;
 }
 
-stu * cut() {
-    return 0;
-}
-
-stu * sort() {
-    return 0;
-}
-
-stu * print() {
+stu *sort() {
     return 0;
 }
 
 //录入信息
-stu * create() {
-    //创建三个结构体 s *head , *stuBefore *stuAfter
-    struct stu *head, *stuBefore, *stuAfter;
-    //给stuBefore, stuAfter分配内存
-    stuBefore= stuAfter = (struct stu *) malloc(sizeof(struct stu));
-    //将head初始化
+stu *create() {
+    //创建三个结构体指针 *head,*beforeStu,*AfterStu
+    struct stu *head, *beforeStu, *afterStu;
     head = NULL;
-    //获取用户输入的学号和成绩
-    printf("请输入学号：");
-    scanf("%d", &stuBefore->num);
-    printf("请输入成绩：");
-    scanf("%f", &stuBefore->score);
-    //如果学号为0则退出while循环
-    int flageNum = 0;
-    while (stuBefore->num){
-        //创建一个标记，每次循环都将标记置为0
-        flageNum ++;
-        //判断标记是不是为1
-        if (flageNum == 1){
-            //如果为1则将head指向stuBefore
-            head = stuBefore;
+    //分配内存
+    beforeStu = afterStu = (struct stu *) malloc(sizeof(struct stu));
+    //将输入传给beforeStu
+    printf("请输入学号和成绩：");
+    scanf("%d", &beforeStu->num);
+    scanf("%f", &beforeStu->score);
+    //如果while循环中的flageCnt == 1的话，将head指针指向beforeStu
+    int flageCnt = 0;
+    while (beforeStu->num) {
+        flageCnt++;
+        if (flageCnt == 1) {
+            head = beforeStu;
         } else {
-            //如果不为1则将stuAfter指向stuBefore
-            stuAfter->next = stuBefore;
+            beforeStu = afterStu;
         }
-//     给stuBefor从新开辟一块内存
-        stuBefore = (struct stu *) malloc(sizeof(struct stu));
-        //将stuAfer的next指向stuBefore。第一次执行时，stuAfter->next = stuBefore 就等于 head->next = stuBefore。因为他们的内存地址是一样的
-        stuAfter->next = stuBefore;
-        //获取用户输入的学号和成绩
-        printf("请输入学号：");
-        scanf("%d", &stuBefore->num);
-        printf("请输入成绩：");
-        scanf("%f", &stuBefore->score);
+        //为befor分配内存
+        afterStu = (struct stu*)malloc(sizeof(struct stu));
+        //将afterStu的next指针指向beforeStu
+        beforeStu->next = afterStu;
+        //赋值给beforeStu
+        printf("请输入学号和成绩：");
+        scanf("%d", &afterStu->num);
+        scanf("%f", &afterStu->score);
     }
-    //将stuAfter的next指向NULL
-    stuAfter->next = NULL;
+    beforeStu->next = NULL;
     return head;
+}
+
+stu *print(stu *pStu) {
+    struct stu *p;
+    p = pStu;
+    while (p != NULL) {
+        printf("学号是：%d", p->num);
+        printf("成绩是：%f", p->score);
+        printf("/n");
+        p = p->next;
+    }
+}
+
+//修改信息
+stu *cut(stu *pStu) {
+    //获取学号
+    //遍历链表
+    //找到学号
+    //修改信息
+    //重新指向
+    return 0;
 }
 
 void menu() {
@@ -74,15 +80,15 @@ void menu() {
     printf("若想录入信息请输入1\n\n若想显示已录入信息请输入2\n\n若想修改（删除）某个学生的信息并显示请输入3\n\n");
     printf("若想修改（增加）信息并显示请输入4\n\n若想查询某一个学生的成绩请输入5\n\n若想成绩排序请输入6（由大到小）\n\n登出系统请输入0\n\n");
     printf("请选择您需要的操作：");
-    int countSystemCnt,inputNumber,j = 0;
+    int countSystemCnt, inputNumber, j = 0;
     struct stu *returnStu;
     scanf("%d", &inputNumber);
-    while (inputNumber != 0){
-        if (inputNumber != 1 && countSystemCnt == 0){
-            while (inputNumber != 1){
+    while (inputNumber != 0) {
+        if (inputNumber != 1 && countSystemCnt == 0) {
+            while (inputNumber != 1) {
                 printf("请先录入信息\n");
                 scanf("%d", &inputNumber);
-                if (inputNumber == 0){
+                if (inputNumber == 0) {
                     goto end;
                 }
             }
@@ -92,10 +98,10 @@ void menu() {
                 returnStu = create();
                 break;
             case 2:
-                returnStu = print();
+                returnStu = print(returnStu);
                 break;
             case 3:
-                returnStu = cut();
+                returnStu = cut(returnStu);
                 break;
             case 4:
                 returnStu = add();
@@ -110,15 +116,14 @@ void menu() {
         printf("请选择您需要的操作：");
         scanf("%d", &inputNumber);
         printf("\n");
-        countSystemCnt ++;
+        countSystemCnt++;
     }
     end:
     printf("已退出系统\n");
     //测试
 }
 
-int main()
-{
+int main() {
     menu();
     return 0;
 }
